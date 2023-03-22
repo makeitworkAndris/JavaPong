922162efa3732aacacf57d20cbaef6b61f574947
makeitworkAndris <andrisbarath5@gmail.com>
2023-03-22 18:30:25 +0100

    Created window with swing.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    static JFrame frame = new JFrame("Pong");
+
+    public static void main(String[] args) {
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(650,495);
+        frame.setVisible(true);
+    }
+}
