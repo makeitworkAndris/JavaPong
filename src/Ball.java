b8157afa0a13669d99b7811ff939342ea9c4b853
makeitworkAndris <andrisbarath5@gmail.com>
2023-03-22 19:04:05 +0100

    Created the Constructor for the Ball and added it to the PongGame Class.

--- src/Ball.java
+++ src/Ball.java
@@ -1,2 +1,20 @@
+import java.awt.*;
+
 public class Ball {
+    private int x,y,cx,cy,size,speed;
+    private Color color;
+
+    public Ball (int x, int y, int cx, int cy, int size, int speed, Color color){
+        this.x = x;
+        this.y = y;
+        this.cx = cx;
+        this.cy = cy;
+        this.size = size;
+        this.speed = speed;
+        this.color = color;
+    }
+    public void paint(Graphics g){
+        g.setColor(color);
+        g.fillOval(x,y,size,size);
+    }
 }
--- src/PongGame.java
+++ src/PongGame.java
@@ -4,9 +4,14 @@ import java.awt.*;
 public class PongGame extends JPanel{
 
     static final int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 640;
-
     public void paintComponent(Graphics g){
-        g.setColor(Color.black);
+        g.setColor(Color.BLACK);
         g.fillRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+        gameBall.paint(g);
+    }
+    private Ball gameBall;
+
+    public PongGame(){
+        gameBall = new Ball(300,200,3,3,10,3,Color.YELLOW);
     }
 }
