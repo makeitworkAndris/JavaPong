e9dc0f4e16d20c75e22e3c6c28d0e7b6d230d854
makeitworkAndris <andrisbarath5@gmail.com>
2023-03-22 18:47:14 +0100

    Set color and size to PongGame class. Created a PongGame object and added it to the frame.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(650,495);
+
+        PongGame game = new PongGame();
+        frame.add(game);
+
         frame.setVisible(true);
     }
 }
--- src/PongGame.java
+++ src/PongGame.java
@@ -1,2 +1,12 @@
-public class PongGame {
+import javax.swing.*;
+import java.awt.*;
+
+public class PongGame extends JPanel{
+
+    static final int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 640;
+
+    public void paintComponent(Graphics g){
+        g.setColor(Color.black);
+        g.fillRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+    }
 }
