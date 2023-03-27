4fe5666011e2773aee8ac07d3ea6bf7ed398453e
makeitworkAndris <andrisbarath5@gmail.com>
2023-03-27 13:34:12 +0200

    Created Paddle class, And made PongGame class to draw them. (Paddles are not functional.)

--- src/PongGame.java
+++ src/PongGame.java
@@ -7,15 +7,26 @@ public class PongGame extends JPanel{
     public void paintComponent(Graphics g){
         g.setColor(Color.BLACK);
         g.fillRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+
         gameBall.paint(g);
+
+        userPaddle.paint(g);
+        pcPaddle.paint(g);
+
     }
     private Ball gameBall;
+    private Paddle userPaddle,pcPaddle;
 
     public PongGame(){
-        gameBall = new Ball(300,200,3,3,10,3,Color.YELLOW);
+        gameBall = new Ball(200,200,3,3,10,3,Color.YELLOW);
+        userPaddle = new Paddle(5,180,75,3,Color.RED);
+        pcPaddle = new Paddle(620,180,75,3,Color.BLUE);
     }
     public void gameLogic(){
         gameBall.moveBall();
         gameBall.bounceOffEdges(0,WINDOW_HEIGHT);
     }
+
+
+
 }
