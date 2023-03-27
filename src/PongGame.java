d7a56e0865877976a1f6678f3e14d45f4064dd41
makeitworkAndris <andrisbarath5@gmail.com>
2023-03-27 15:49:28 +0200

    Added scoreboard.

--- src/PongGame.java
+++ src/PongGame.java
@@ -10,7 +10,20 @@ public class PongGame extends JPanel implements MouseMotionListener {
     private Paddle userPaddle,pcPaddle;
     private int userMouseY;
     private int bounceCount;
+    private int userScore;
+    private int pcScore;
 
+    public PongGame(){
+        gameBall = new Ball(200,200,3,3,10,3,Color.YELLOW);
+        userPaddle = new Paddle(5,180,75,3,Color.RED);
+        pcPaddle = new Paddle(620,180,75,3,Color.BLUE);
+
+        userMouseY = 0;
+        userScore = 0;
+        pcScore = 0;
+
+        addMouseMotionListener(this);
+    }
 
     public void paintComponent(Graphics g){
         g.setColor(Color.BLACK);
@@ -21,17 +34,13 @@ public class PongGame extends JPanel implements MouseMotionListener {
         userPaddle.paint(g);
         pcPaddle.paint(g);
 
+        g.setColor(Color.WHITE);
+        g.drawString("Score - User [ " + userScore + "] PC [ "+pcScore + "]", 250, 20);
+
     }
 
 
-    public PongGame(){
-        gameBall = new Ball(200,200,3,3,10,3,Color.YELLOW);
-        userPaddle = new Paddle(5,180,75,3,Color.RED);
-        pcPaddle = new Paddle(620,180,75,3,Color.BLUE);
 
-        userMouseY = 0;
-        addMouseMotionListener(this);
-    }
     public void gameLogic(){
         gameBall.moveBall();
         gameBall.bounceOffEdges(0,WINDOW_HEIGHT);
