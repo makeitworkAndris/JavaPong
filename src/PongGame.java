8411f3cd6ec180971b99114a26df0096ad258166
makeitworkAndris <andrisbarath5@gmail.com>
2023-03-27 13:53:22 +0200

    Added mouse tracking for the userPaddle

--- src/PongGame.java
+++ src/PongGame.java
@@ -1,9 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
-public class PongGame extends JPanel{
+public class PongGame extends JPanel implements MouseMotionListener {
 
     static final int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 460;
+    private Ball gameBall;
+    private Paddle userPaddle,pcPaddle;
+
+    private int userMouseY;
+
+
     public void paintComponent(Graphics g){
         g.setColor(Color.BLACK);
         g.fillRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
@@ -14,21 +22,31 @@ public class PongGame extends JPanel{
         pcPaddle.paint(g);
 
     }
-    private Ball gameBall;
-    private Paddle userPaddle,pcPaddle;
+
 
     public PongGame(){
         gameBall = new Ball(200,200,3,3,10,3,Color.YELLOW);
         userPaddle = new Paddle(5,180,75,3,Color.RED);
         pcPaddle = new Paddle(620,180,75,3,Color.BLUE);
+
+        userMouseY = 0;
+        addMouseMotionListener(this);
     }
     public void gameLogic(){
         gameBall.moveBall();
         gameBall.bounceOffEdges(0,WINDOW_HEIGHT);
-        userPaddle.moveTowards(0);
-        pcPaddle.moveTowards(600);
+        userPaddle.moveTowards(userMouseY);
+
     }
 
 
+    @Override
+    public void mouseDragged(MouseEvent e) {
 
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        userMouseY = e.getY();
+    }
 }
